71fe75d3c7a32ca35cb68ec398983f84d87b32db
Caspian Baska <email@caspian.computer>
2021-09-24 10:21:11 +1000

    test(generator): set parent type of settings

--- spec/settings_spec.cr
+++ spec/settings_spec.cr
@@ -26,14 +26,12 @@ module PlaceOS::Model
 
     it "ensures settings_string cannot be an array" do
       settings = Generator.settings(settings_string: "['apple','orange']")
-      settings.save
       settings.valid?.should be_false
       settings.errors.first.to_s.should eq "settings_string is not a valid JSON or YAML object"
     end
 
     it "accepts a valid settings_string" do
       settings = Generator.settings(settings_string: %(screen: 1))
-      settings.save
       settings.valid?.should be_true
       settings.settings_json.should eq %({"screen":1})
     end
