09966d7ca39e6c96ad741cafc77f832dead5f6b0
tassja <xtassja@gmail.com>
2021-12-14 12:27:30 +1100

    style(repository): add description

--- src/placeos-models/repository.cr
+++ src/placeos-models/repository.cr
@@ -23,6 +23,7 @@ module PlaceOS::Model
     attribute commit_hash : String = "HEAD"
     attribute branch : String = "master"
 
+    # Whether the repository is pinned to a release asset
     attribute release : Bool = false
 
     # Authentication
