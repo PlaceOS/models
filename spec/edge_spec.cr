4d33d73b36739c700c650c79bf028b34f57d16c3
Caspian Baska <email@caspian.computer>
2022-02-22 14:58:40 +1100

    refactor(edge): move key clean-up to `save!`

--- spec/edge_spec.cr
+++ spec/edge_spec.cr
@@ -2,15 +2,8 @@ require "./helper"
 
 module PlaceOS::Model
   describe Edge do
-    it ".create" do
-      create_body = Edge::CreateBody.new(Faker::Name.name)
-      user = Generator.user.save!
-      Edge.create(create_body, user)
-    end
-
     it "saves an Edge" do
       edge = Generator.edge.save!
-
       edge.should_not be_nil
       edge.persisted?.should be_true
       Edge.find!(edge.id.as(String)).id.should eq edge.id
