f900f65827953a6a6b77596126e618f0174b6106
Stephen von Takach <steve@place.technology>
2022-04-26 20:00:18 +1000

    only expose staff_id

--- src/placeos-models/user.cr
+++ src/placeos-models/user.cr
@@ -306,13 +306,14 @@ module PlaceOS::Model
     PUBLIC_DATA = [
       :email_digest, :nickname, :name, :first_name, :last_name, :groups,
       :country, :building, :image, :created_at, :authority_id, :deleted,
-      :department, :preferred_language, :staff_id, :email, :phone, :ui_theme,
+      :department, :preferred_language, :staff_id,
     ]
 
     {% begin %}
     ADMIN_DATA = {{
                    PUBLIC_DATA + [
                      :sys_admin, :support, :misc, :login_name, :card_number,
+                     :email, :phone, :ui_theme,
                    ]
                  }}
     {% end %}
