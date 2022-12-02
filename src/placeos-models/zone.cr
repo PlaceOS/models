51d062ed2c8d462309b9995cd4be61921790d523
Mia Bennett <mia@chillfox.com>
2022-12-02 14:50:22 +0930

    feat(zone): add images

--- src/placeos-models/zone.cr
+++ src/placeos-models/zone.cr
@@ -38,6 +38,7 @@ module PlaceOS::Model
     # =============================
 
     attribute triggers : Array(String) = [] of String
+    attribute images : Array(String) = [] of String
 
     # Association
     ###############################################################################################
