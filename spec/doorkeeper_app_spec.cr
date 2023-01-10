ec0fecbb711aa44a1ac8296036616654b7c2e79f
Ali Naqvi <syed.alinaqvi@gmail.com>
2023-01-10 13:30:37 +0800

    update: changed DoorkeeperApplication to use oauth_applications as its table

--- spec/doorkeeper_app_spec.cr
+++ spec/doorkeeper_app_spec.cr
@@ -6,7 +6,6 @@ module PlaceOS::Model
       app = DoorkeeperApplication.new
       app.name = RANDOM.hex(10)
       app.redirect_uri = "http://test.redirect.com.au/#{RANDOM.hex(3)}"
-      app.owner_id = RANDOM.hex(10)
 
       begin
         app.save!
@@ -25,12 +24,10 @@ module PlaceOS::Model
         app1 = DoorkeeperApplication.new
         app1.name = RANDOM.hex(10)
         app1.redirect_uri = uri
-        app1.owner_id = RANDOM.hex(10)
         app1.save!
         app2 = DoorkeeperApplication.new
         app2.name = RANDOM.hex(10)
         app2.redirect_uri = uri
-        app2.owner_id = RANDOM.hex(10)
         app2.save!
       end
     end
@@ -41,12 +38,10 @@ module PlaceOS::Model
         app1 = DoorkeeperApplication.new
         app1.name = name
         app1.redirect_uri = "appuri://test.redirect.com.au/#{RANDOM.hex(3)}"
-        app1.owner_id = RANDOM.hex(10)
         app1.save!
         app2 = DoorkeeperApplication.new
         app2.name = name
         app2.redirect_uri = "appuri://test.redirect.com.au/#{RANDOM.hex(3)}"
-        app2.owner_id = RANDOM.hex(10)
         app2.save!
       end
     end
@@ -55,7 +50,6 @@ module PlaceOS::Model
       app = DoorkeeperApplication.new
       app.name = RANDOM.hex(10)
       app.redirect_uri = "appuri://test.redirect.com.au/#{RANDOM.hex(3)}"
-      app.owner_id = RANDOM.hex(10)
 
       begin
         app.save!
@@ -73,7 +67,6 @@ module PlaceOS::Model
       app.name = RANDOM.hex(10)
       app.redirect_uri = "http://test.redirect.com.au/#{RANDOM.hex(3)}"
       app.uid = "my-special-uid"
-      app.owner_id = RANDOM.hex(10)
 
       begin
         app.save!
