c79b9ee86e53e3ae6aa2162d4b762a64bacf3abc
Stephen von Takach <steve@place.technology>
2023-05-10 13:07:48 +1000

    feat(migrations): add db level constraints to trig table (#203)

---
+++ migration/db/migrations/20230510220259_alter_trig_table.sql
@@ -0,0 +1,24 @@
+-- +micrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+ALTER TABLE "trig"
+ALTER COLUMN control_system_id SET NOT NULL,
+ALTER COLUMN trigger_id SET NOT NULL,
+
+ADD CONSTRAINT fk_control_system
+FOREIGN KEY (control_system_id)
+REFERENCES sys(id)
+ON DELETE CASCADE,
+
+ADD CONSTRAINT fk_trigger
+FOREIGN KEY (trigger_id)
+REFERENCES triggers(id)
+ON DELETE CASCADE;
+
+-- +micrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+ALTER TABLE "trig"
+DROP CONSTRAINT fk_control_system,
+DROP CONSTRAINT fk_trigger,
+
+ALTER COLUMN control_system_id DROP NOT NULL,
+ALTER COLUMN trigger_id DROP NOT NULL;
