f0ead335bc1558f96e181821969675e0e4bdc3b8
Mia Bennett <mia@chillfox.com>
2023-05-22 13:45:40 +0930

    feat(asset): [PPT-334] elastic search index (#207)
    * feat(asset): [PPT-334] elastic search index
    
    * changed name es to es_subfield
    
    ---------
    
    Co-authored-by: Ali Naqvi <syed.alinaqvi@gmail.com>

--- src/placeos-models/asset_category.cr
+++ src/placeos-models/asset_category.cr
@@ -8,7 +8,7 @@ module PlaceOS::Model
     table :asset_category
 
     # i.e. a tablet
-    attribute name : String
+    attribute name : String, es_subfield: "keyword"
     attribute description : String?
     # attribute parent_category_id : Int64?
 
