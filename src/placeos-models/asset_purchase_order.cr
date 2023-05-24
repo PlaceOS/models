57afd6551bda690fa4739b23675bc382851f7e2c
Mia Bennett <mia@chillfox.com>
2023-05-24 13:03:49 +0930

    feat(asset_manager): change ids to strings (#208)
    * feat(asset_manager): change ids to strings
    
    * refactor(asset_manager): add if exist to drops
    
    * feat(asset_manager): change time fields to int64

--- src/placeos-models/asset_purchase_order.cr
+++ src/placeos-models/asset_purchase_order.cr
@@ -2,7 +2,7 @@ require "./base/model"
 require "./asset"
 
 module PlaceOS::Model
-  class AssetPurchaseOrder < ModelWithAutoKey
+  class AssetPurchaseOrder < ModelBase
     include PlaceOS::Model::Timestamps
 
     table :asset_purchase_order
@@ -10,16 +10,21 @@ module PlaceOS::Model
     attribute purchase_order_number : String, es_type: "keyword"
     attribute invoice_number : String?
     attribute supplier_details : JSON::Any?
-    attribute purchase_date : Time?
+    attribute purchase_date : Int64?
 
     attribute unit_price : Int64?
-    attribute expected_service_start_date : Time?
-    attribute expected_service_end_date : Time?
+    attribute expected_service_start_date : Int64?
+    attribute expected_service_end_date : Int64?
 
     has_many(
       child_class: Asset,
       foreign_key: "purchase_order_id",
       collection_name: :assets
     )
+
+    # Validation
+    ###############################################################################################
+
+    validates :purchase_order_number, presence: true
   end
 end
