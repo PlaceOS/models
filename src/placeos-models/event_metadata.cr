3e15ad19864b774aea8f5c20a0d22f5adac4eb49
Stephen von Takach <steve@place.technology>
2023-05-31 08:00:09 +1000

    fix spelling

--- src/placeos-models/event_metadata.cr
+++ src/placeos-models/event_metadata.cr
@@ -93,7 +93,7 @@ module PlaceOS::Model
           end
 
           # reject clashing bookings
-          Booking.where({:id => classhing.map(&.id)}).update_all({:rejected => true, :rejected_at => Time.utc.to_unix}) unless clashing.empty?
+          Booking.where({:id => clashing.map(&.id)}).update_all({:rejected => true, :rejected_at => Time.utc.to_unix}) unless clashing.empty?
 
           # ensure the booking times are in sync
           Booking.where(event_id: id).update_all({:booking_start => event_start, :booking_end => event_end})
