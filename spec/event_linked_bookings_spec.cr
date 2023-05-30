3eeb9915890caeb0976080260a3970c0addfedc4
Stephen von Takach <steve@place.technology>
2023-05-30 16:30:21 +1000

    user get_tenant helper

--- spec/event_linked_bookings_spec.cr
+++ spec/event_linked_bookings_spec.cr
@@ -2,10 +2,9 @@ require "./helper"
 
 module PlaceOS::Model
   describe Booking do
-    tenant = Generator.tenant
-
     describe "event metadata linked bookings" do
       it "creates a booking linked to event metadata" do
+        tenant = get_tenant
         event_start = 5.minutes.from_now
         event_end = 10.minutes.from_now
         asset_id = "chair"
