606db0672ecb2171b7027db018b5c942c864b21e
Stephen von Takach <steve@place.technology>
2023-05-30 16:09:30 +1000

    add initial spec

---
+++ spec/event_linked_bookings_spec.cr
@@ -0,0 +1,22 @@
+require "./helper"
+
+module PlaceOS::Model
+  describe Booking do
+    tenant = Generator.tenant
+
+    describe "event metadata linked bookings" do
+      it "creates a booking linked to event metadata" do
+        event_start = 5.minutes.from_now
+        event_end = 10.minutes.from_now
+        asset_id = "chair"
+
+        event = Generator.event_metadata(tenant.id, event_start, event_end)
+        event.save!
+        booking = Generator.booking(tenant.id, asset_id, event_start, event_end, event_id: event.id)
+        booking.save!
+
+        event.bookings.first?.try(&.id).should eq booking.id
+      end
+    end
+  end
+end
