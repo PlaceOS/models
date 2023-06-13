b636e88e76956d2b89c6bd90af52bfd4de235bab
Stephen von Takach <steve@place.technology>
2023-06-13 20:36:06 +1000

    remove unique constraint at db level

--- migration/db/migrations/20230613132534_multiple_tenants_per_domain.sql
+++ migration/db/migrations/20230613132534_multiple_tenants_per_domain.sql
@@ -2,8 +2,25 @@
 -- SQL in section 'Up' is executed when this migration is applied
 
 ALTER TABLE tenants ADD COLUMN IF NOT EXISTS email_domain TEXT;
+
+DO $$
+DECLARE 
+    constraint_name VARCHAR;
+BEGIN
+    SELECT conname INTO constraint_name
+    FROM pg_constraint 
+    INNER JOIN pg_class ON conrelid=pg_class.oid 
+    INNER JOIN pg_attribute ON pg_attribute.attnum=conkey[1] 
+    WHERE relname='tenants' AND attname='domain';
+
+    IF constraint_name IS NOT NULL THEN
+        EXECUTE format('ALTER TABLE tenants DROP CONSTRAINT %I', constraint_name);
+    END IF;
+END $$;
  
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
 ALTER TABLE tenants DROP COLUMN email_domain;
+ALTER TABLE tenants
+ADD CONSTRAINT tenants_domain_key UNIQUE(domain);
