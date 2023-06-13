da0e6658492a50720b9d85bdb264f0c5a17141f9
Stephen von Takach <steve@place.technology>
2023-06-13 20:49:46 +1000

    missing semi

--- migration/db/migrations/20230613132534_multiple_tenants_per_domain.sql
+++ migration/db/migrations/20230613132534_multiple_tenants_per_domain.sql
@@ -2,7 +2,7 @@
 -- SQL in section 'Up' is executed when this migration is applied
 
 ALTER TABLE tenants ADD COLUMN IF NOT EXISTS email_domain TEXT;
-ALTER TABLE tenants DROP CONSTRAINT tenants_domain_key
+ALTER TABLE tenants DROP CONSTRAINT tenants_domain_key;
  
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
