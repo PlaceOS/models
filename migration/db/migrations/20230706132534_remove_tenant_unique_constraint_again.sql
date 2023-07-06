0f27512b270f2aa5af76f7472be4e32cb12c903b
Stephen von Takach <steve@place.technology>
2023-07-06 12:49:34 +1000

    fix(migrations): ensure unique_domain constraint does not exist

---
+++ migration/db/migrations/20230706132534_remove_tenant_unique_constraint_again.sql
@@ -0,0 +1,22 @@
+-- +micrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM   information_schema.table_constraints 
+        WHERE  table_name   = 'tenants' 
+        AND    constraint_name = 'unique_domain'
+    )
+    THEN
+        ALTER TABLE tenants
+        DROP CONSTRAINT unique_domain;
+    END IF;
+END $$;
+
+DROP INDEX concurrently IF EXISTS unique_domain;
+DROP INDEX IF EXISTS unique_domain;
+  
+-- +micrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
