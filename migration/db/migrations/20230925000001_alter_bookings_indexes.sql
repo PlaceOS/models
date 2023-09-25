5249ea1dcf259af53809b73c58457c8fb2c67b58
Stephen von Takach <steve@place.technology>
2023-09-25 18:00:37 +1000

    feat(migrations): move id indexes to hash indexes
    as these have better performance and use less disk space

--- migration/db/migrations/20230925000001_alter_bookings_indexes.sql
+++ migration/db/migrations/20230925000001_alter_bookings_indexes.sql
@@ -1,9 +1,23 @@
 -- +micrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-CREATE INDEX IF NOT EXISTS bookings_parent_id_idx ON bookings (parent_id);
+CREATE INDEX IF NOT EXISTS bookings_booked_by_id_idx ON bookings USING HASH (booked_by_id);
+
+DROP INDEX IF EXISTS bookings_parent_id_idx;
+CREATE INDEX IF NOT EXISTS bookings_parent_id_idx ON bookings USING HASH (parent_id);
+
+-- re-create some existing indexes for improved performance
+DROP INDEX IF EXISTS index_bookings_booking_email_digest_idx;
+CREATE INDEX IF NOT EXISTS bookings_email_digest_idx ON bookings USING HASH (email_digest);
+
+DROP INDEX IF EXISTS index_bookings_booking_user_id_idx;
+CREATE INDEX IF NOT EXISTS bookings_user_id_idx ON bookings USING HASH (user_id);
+
+DROP INDEX IF EXISTS index_bookings_event_id_idx;
+CREATE INDEX IF NOT EXISTS bookings_event_id_idx ON bookings USING HASH (event_id);
+
+DROP INDEX IF EXISTS index_bookings_tenant_id;
+CREATE INDEX IF NOT EXISTS bookings_tenant_id_idx ON bookings USING HASH (tenant_id);
 
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-
-DROP INDEX IF EXISTS bookings_parent_id_idx;
