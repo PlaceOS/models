bc906ef28fc27e27eae1a9561a7583553fb8afd8
Ali Naqvi <syed.alinaqvi@gmail.com>
2023-12-12 16:37:55 +0800

    fix(booking_spec): minor fixes to booking_spec

--- spec/booking_spec.cr
+++ spec/booking_spec.cr
@@ -18,8 +18,9 @@ module PlaceOS::Model
       booking.attendees.size.should eq 1
 
       # ensure we didn't need a second query to fill this in
-      guests = booking.@__guests_rel.as(Array(Guest))
+      guests = booking.__guests_rel
       guests.size.should eq 1
+      JSON.parse(booking.to_json).as_h["guests"].as_a.size.should eq 1
 
       booking.attendees.first.destroy
       query_check = Booking.where(id: booking_id).join(:left, Attendee, :booking_id).join(:left, Guest, "guests.id = attendees.guest_id").to_a.first
