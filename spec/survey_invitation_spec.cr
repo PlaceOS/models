28c9503488c06ef6be0ea38d0cb7718bf218c34d
Mia Bennett <mia@chillfox.com>
2024-02-21 13:59:51 +0930

    test(survey): list invitations

--- spec/survey_invitation_spec.cr
+++ spec/survey_invitation_spec.cr
@@ -15,5 +15,38 @@ module PlaceOS::Model
       invitation.persisted?.should be_true
       Survey::Invitation.find!(invitation.id).id.should eq invitation.id
     end
+
+    it "lists invitations" do
+      invitation_one = Generator.invitation(sent: true).save!
+      invitation_two = Generator.invitation(sent: false).save!
+      invitation_three = Generator.invitation(sent: nil).save!
+
+      surveys = Survey::Invitation.list(sent: nil)
+      surveys.size.should eq 3
+      surveys.map(&:id).should contain(invitation_one.id)
+      surveys.map(&:id).should contain(invitation_two.id)
+      surveys.map(&:id).should contain(invitation_three.id)
+    end
+
+    it "lists invitations with sent = true" do
+      invitation_one = Generator.invitation(sent: true).save!
+      invitation_two = Generator.invitation(sent: false).save!
+      invitation_three = Generator.invitation(sent: nil).save!
+
+      surveys = Survey::Invitation.list(sent: true)
+      surveys.size.should eq 1
+      surveys.first.id.should eq invitation_one.id
+    end
+
+    it "lists invitations with sent != true" do
+      invitation_one = Generator.invitation(sent: true).save!
+      invitation_two = Generator.invitation(sent: false).save!
+      invitation_three = Generator.invitation(sent: nil).save!
+
+      surveys = Survey::Invitation.list(sent: false)
+      surveys.size.should eq 2
+      surveys.map(&:id).should contain(invitation_two.id)
+      surveys.map(&:id).should contain(invitation_three.id)
+    end
   end
 end
