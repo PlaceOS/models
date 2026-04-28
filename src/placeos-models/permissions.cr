3663a67a4cccecf06ca4eb7533cc81fdcdaf7826
Stephen von Takach <steve@place.technology>
2026-04-28 18:19:46 +1000

    chore(permissions): add some comments

--- src/placeos-models/permissions.cr
+++ src/placeos-models/permissions.cr
@@ -6,13 +6,13 @@ module PlaceOS::Model
   # (also how a deny-only GroupZone with no bits set would read — a no-op deny).
   @[Flags]
   enum Permissions : Int32
-    Read
-    Create
-    Update
-    Delete
+    Read    # list / show (not always applicable to each controller)
+    Create  # POST new database entries
+    Update  # PUT / PATCH updates
+    Delete  # DELETE entries (destroy model)
     Operate # execute functions
-    Approve
-    Manage # similar to a system admin for the groups and models this application exposes
-    Share  # can grant other groups and externals access
+    Approve # approve changes, such as signage playlist updates
+    Manage  # similar to a system admin for the groups and models this application exposes
+    Share   # can grant other groups and externals access
   end
 end
